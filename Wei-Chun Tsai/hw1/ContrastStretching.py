501c05a9d2dbc74d8871959460789fa9d26bbe3b
Kevin Tsai <53148219+KevinTsaiCodes@users.noreply.github.com>
2022-10-18 01:24:58 +0800

    Last Updated

--- Wei-Chun Tsai/hw1/ContrastStretching.py
+++ Wei-Chun Tsai/hw1/ContrastStretching.py
@@ -33,21 +33,6 @@ class ColorModel:
 
         return gray_img
 
-class ContrastStretching:
-
-    def __init__(self):
-        pass
-
-    def normalize(gray_img):
-        gray_intensity = gray_img
-        InP = gray_intensity  # Input pixel value
-        minI = 20  # 可操控值, TODO, minimum pixel value in the input image
-        maxI = 120  # 可操控值, TODO, minimum pixel value in the input image
-        minO = np.min(gray_intensity)  # Minimum pixel value in the output image
-        maxO = np.max(gray_intensity) # Maximum pixel value in the output image
-        OutP = (InP - minI) * ( ( (maxO - minO) / (maxI - minI) ) + minO)  # Output pixel value
-        cv2.imwrite('misc/High-Contrast.jpg', OutP)
-        return OutP
 
 class ContrastStretching:
 
