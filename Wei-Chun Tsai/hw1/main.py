43b88debd2b79d7aa37fd60f16174ff571d0e872
Kevin Tsai <53148219+KevinTsaiCodes@users.noreply.github.com>
2022-10-17 22:28:07 +0800

    updated final main.py (final)

--- Wei-Chun Tsai/hw1/main.py
+++ Wei-Chun Tsai/hw1/main.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import ContrastStretching as CS
 
 file = 'Floor.jpg'
@@ -6,17 +5,4 @@ file = 'Floor.jpg'
 if __name__ == '__main__':
     gray_img = CS.ColorModel.rgb2gray(file)
     output_IMG = CS.ContrastStretching.normalize(gray_img)
-
-    plt.subplot(1, 2, 1)
-    plt.imshow(gray_img, cmap='gray')
-    plt.title('Gray Scale')
-    plt.xticks([])
-    plt.yticks([])
-
-    plt.subplot(2, 2, 2)
-    plt.imshow(output_IMG, cmap='gray')
-    plt.title('Contrast Stretching')
-    plt.xticks([])
-    plt.yticks([])
-    
-    plt.show()
+    CS.VersusImage.plot_image(gray_img, output_IMG)
