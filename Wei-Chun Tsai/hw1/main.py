7ee7fc8861937c876b970c3584f532b4bf6b78cb
Kevin Tsai <53148219+KevinTsaiCodes@users.noreply.github.com>
2022-10-17 18:09:53 +0800

    Final Main.py

--- Wei-Chun Tsai/hw1/main.py
+++ Wei-Chun Tsai/hw1/main.py
@@ -10,22 +10,17 @@ if __name__ == '__main__':
     plt.subplot(1, 2, 1)
     plt.imshow(gray_img, cmap='gray')
     plt.title('Gray Scale')
+    plt.xticks([])
+    plt.yticks([])
 
-    plt.subplot(1, 2, 2)
+    plt.subplot(2, 2, 2)
     plt.imshow(output_IMG, cmap='gray')
     plt.title('Contrast Stretching')
+    plt.xticks([])
+    plt.yticks([])
 
-    plt.show()
-
-    median_img = output_IMG.copy()
-    median_img = IP.Filter.Median(median_img)
-
-    plt.subplot(1, 2, 1)
-    plt.imshow(median_img, cmap='gray')
-    plt.title('Median Filter')
-
-    plt.subplot(1, 2, 2)
-    plt.imshow(output_IMG, cmap='gray')
-    plt.title('Contrast Stretching Results')
+    #  Histogram Plotting of Original Image and Results
+    gray_img = CS.HistogramImage.histogram(gray_img)
+    output_IMG = CS.HistogramImage.histogram(output_IMG)
 
     plt.show()
