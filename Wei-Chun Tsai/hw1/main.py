c7720f263d80692876442ec677464e789c32b6e2
Kevin Tsai <53148219+KevinTsaiCodes@users.noreply.github.com>
2022-11-10 19:32:32 +0800

    FIN main.py

--- Wei-Chun Tsai/hw1/main.py
+++ Wei-Chun Tsai/hw1/main.py
@@ -1,8 +1,11 @@
 import ContrastStretching as CS
 
-file = 'Floor.jpg'
+file = 'Stair.jpg'
 
 if __name__ == '__main__':
     gray_img = CS.ColorModel.rgb2gray(file)
     output_IMG = CS.ContrastStretching.normalize(gray_img)
     CS.VersusImage.plot_image(gray_img, output_IMG)
+    CS.ContrastStretching.histogram(gray_img, output_IMG)
+    entropy = CS.ContrastStretching.CalculateEntropy()
+    print('Entropy of Contrast Stretching Method: ', entropy)
