a4a7864634248552ebcf92c22e4e3bfa7b415573
Kevin Tsai <53148219+KevinTsaiCodes@users.noreply.github.com>
2022-11-24 18:55:39 +0800

    updated

--- Wei-Chun Tsai/hw2/add_noise.m
+++ Wei-Chun Tsai/hw2/add_noise.m
@@ -2,5 +2,5 @@ org_img = imread('Stair.jpg');
 gray_img = rgb2gray(org_img);
 sp = imnoise(gray_img, 'salt & pepper', 0.1);
 gn = imnoise(gray_img, 'gaussian');
-imwrite(sp, 'D:\Matlab_ImageProcessing\salt-and-pepper-noise.jpg');
-imwrite(gn, 'D:\Matlab_ImageProcessing\gaussian-noise.jpg');
+imwrite(sp, 'salt-and-pepper-noise.jpg');
+imwrite(gn, 'gaussian-noise.jpg');
