0c0bcb24162005a31339e47e7b53e22311aeee39
Tim Huang <91259748+youting83@users.noreply.github.com>
2022-11-24 15:27:41 +0800

    Create Median_blur.py

---
+++ You-Ting Huang/HW2/Median_blur.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('High-Contrast.jpg')
+blur = cv2.medianBlur(img, 5)#Tranfer to 5X5
+blur = cv2.medianBlur(img, 3)#Tranfer to 3X3
+blur = cv2.medianBlur(img, 3)
+
+plt.subplot(131), plt.imshow(img), plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(132), plt.imshow(blur), plt.title('one pass - 5X5')
+plt.xticks([]), plt.yticks([])
+plt.subplot(133), plt.imshow(blur), plt.title('two pass  -3X3')
+plt.xticks([]), plt.yticks([])
+plt.tight_layout()
+plt.show()
